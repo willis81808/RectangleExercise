e5e12903f3e53953a396b491af9f04e0ed29a33c
willis81808 <willis81808@gmail.com>
2021-06-25 05:14:41 -0600

    made rectangle instantiation more robust against out-of-order values

--- src/main/java/com/willis/erick/models/Rectangle.java
+++ src/main/java/com/willis/erick/models/Rectangle.java
@@ -8,10 +8,10 @@ public class Rectangle implements IDrawable {
     public int x1, y1, x2, y2;
 
     public Rectangle(int x1, int y1, int x2, int y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+        this.x1 = Math.min(x1, x2);
+        this.y1 = Math.min(y1, y2);
+        this.x2 = Math.max(x1, x2);
+        this.y2 = Math.max(y1, y2);
     }
 
     public int width() {
